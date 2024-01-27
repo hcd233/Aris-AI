b1361bc76ba7a94ad72e2dd25ab9dde00e656a7b
centonhuang <centonhuang@tencent.com>
2024-01-27 18:04:05 +0800

    🦄 refactor(api): add `code` field

--- internal/api/base.py
+++ internal/api/base.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 
 
 class StandardResponse(BaseModel):
+    code: int
     status: Literal["success", "error"]
-    message: str | None
-    data: Dict[str, Any] | None
+    message: str | None = None
+    data: Dict[str, Any] | None = None
