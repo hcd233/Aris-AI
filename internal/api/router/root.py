e3714b75793cd5e2d6ead0e526325a0b89da7f96
centonhuang <centonhuang@tencent.com>
2024-01-27 18:07:09 +0800

    ✨ feat(api): add new field `code` to response template

--- internal/api/router/root.py
+++ internal/api/router/root.py
@@ -7,4 +7,8 @@ root_router = APIRouter(prefix="/root", tags=["root"])
 
 @root_router.get("/", tags=["root"])
 async def root():
-    return StandardResponse(status="success", message="Welcome to alice ai api!", data=None)
+    return StandardResponse(
+        code=0,
+        status="success",
+        message="Welcome to alice ai api!",
+    )
