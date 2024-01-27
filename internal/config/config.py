3e92b514941a5c1d7e80070b18dec7eba3011eeb
centonhuang <centonhuang@tencent.com>
2024-01-27 18:06:42 +0800

    ✨ feat(api): add mysql env var

--- internal/config/config.py
+++ internal/config/config.py
@@ -6,3 +6,9 @@ LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
 LOG_ROOT = os.environ.get("LOG_ROOT", "./log")
 
 API_PORT = int(os.environ.get("API_PORT", "8000"))
+
+MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE")
+MYSQL_HOST = os.environ.get("MYSQL_HOST")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+MYSQL_PORT = int(os.environ.get("MYSQL_PORT", "3306"))
+MYSQL_USER = os.environ.get("MYSQL_USER")
