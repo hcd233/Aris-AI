3eff83e53b38a7162b75c169d9e00aaf83d42285
centonhuang <centonhuang@tencent.com>
2024-01-27 14:54:19 +0800

    🦄 refactor(api): tiny refactor

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from internal.config import API_PORT
 
 
 def main() -> None:
-    uvicorn.run("internal.api:create_app", host="0.0.0.0", port=API_PORT, reload=True)
+    # same as from internal.api import create_app
+    uvicorn.run(
+        "internal.api:create_app",
+        host="0.0.0.0",
+        port=API_PORT,
+        reload=True,
+    )
 
 
 if __name__ == "__main__":
