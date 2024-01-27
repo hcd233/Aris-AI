252f201280055f50effd2a32b65b99bf9c90e2f8
centonhuang <centonhuang@tencent.com>
2024-01-27 15:09:08 +0800

    🦄 refactor(api): change logger init logic

--- internal/logger/__init__.py
+++ internal/logger/__init__.py
@@ -1,3 +1,30 @@
-from .logger import init_logger
+from pathlib import Path
+from sys import stdout
+
+from loguru import _Logger
+from loguru import logger as _logger
+
+from internal.config import LOG_LEVEL, LOG_ROOT
+
+from .logger import ERROR_LOG, INFO_LOG, LOG_FORMAT
+
+
+def init_logger() -> _Logger:
+    log_root = Path(LOG_ROOT)
+
+    if not log_root.exists():
+        log_root.mkdir(parents=True)
+    if not log_root.is_dir():
+        raise ValueError("LOG_ROOT is not a directory")
+
+    _logger.remove()  # remove origin handler
+    _logger.add(stdout, colorize=True, enqueue=True, level=LOG_LEVEL, format=LOG_FORMAT)
+    _logger.add(log_root.joinpath(INFO_LOG), encoding="utf-8", rotation="10MB", enqueue=True, level="INFO", format=LOG_FORMAT)
+    _logger.add(log_root.joinpath(ERROR_LOG), encoding="utf-8", rotation="10MB", enqueue=True, level="ERROR", format=LOG_FORMAT)
+
+    _logger.info("logger init success")
+
+    return _logger
+
 
 logger = init_logger()
--- internal/logger/logger.py
+++ internal/logger/logger.py
@@ -1,25 +1,5 @@
-from pathlib import Path
-from sys import stdout
+INFO_LOG = "alice-api-info.log"
 
-from loguru import _Logger, logger
+ERROR_LOG = "alice-api-error.log"
 
-from internal.config import LOG_LEVEL, LOG_ROOT
-
-
-def init_logger() -> _Logger:
-    log_root = Path(LOG_ROOT)
-
-    if not log_root.exists():
-        log_root.mkdir(parents=True)
-    if not log_root.is_dir():
-        raise ValueError("LOG_ROOT is not a directory")
-
-    logger.remove()  # remove origin handler
-    logger_format = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level}</level> | <green>MODULE</green>: <cyan>{name}</cyan> - <green>LINE</green>: <cyan>{line}</cyan> - <cyan>[{function}]</cyan>: <level>{message}</level>"
-    logger.add(stdout, colorize=True, enqueue=True, level=LOG_LEVEL, format=logger_format)
-    logger.add(log_root.joinpath("prompt-collector-info.log"), encoding="utf-8", rotation="10MB", enqueue=True, level="INFO", format=logger_format)
-    logger.add(log_root.joinpath("prompt-collector-error.log"), encoding="utf-8", rotation="10MB", enqueue=True, level="ERROR", format=logger_format)
-
-    logger.info("logger init success")
-
-    return logger
+LOG_FORMAT = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level}</level> | <green>MODULE</green>: <cyan>{name}</cyan> - <green>LINE</green>: <cyan>{line}</cyan> - <cyan>[{function}]</cyan>: <level>{message}</level>"
