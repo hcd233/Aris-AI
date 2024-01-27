e6fb68e30ff8aa70769b4b7197d53f2ef905b782
centonhuang <centonhuang@tencent.com>
2024-01-27 14:52:53 +0800

    ✨ feat(api): use uvicorn instead of cli

--- main.py
+++ main.py
@@ -1,29 +1,11 @@
-from typing import Any, Dict, Literal
+import uvicorn
 
-from fastapi import FastAPI
-from internal.logger import logger
-from pydantic import BaseModel
-
-from internal.config import DEBUG_MODE
+from internal.config import API_PORT
 
 
-class StandardResponse(BaseModel):
-    status: Literal["success", "error"]
-    message: str | None
-    data: Dict[str, Any] | None
+def main() -> None:
+    uvicorn.run("internal.api:create_app", host="0.0.0.0", port=API_PORT, reload=True)
 
 
-app = FastAPI(
-    title="Alice AI API",
-    version="0.1.0",
-    # forbidden to route any docs
-    redoc_url="/redoc" if DEBUG_MODE else None,
-    docs_url="/docs" if DEBUG_MODE else None,
-    openapi_url="/openapi.json" if DEBUG_MODE else None,
-)
-logger.info("App initialized")
-
-
-@app.get("/", tags=["root"])
-async def root():
-    return StandardResponse(status="success", message="Hello World!", data=None)
+if __name__ == "__main__":
+    main()
