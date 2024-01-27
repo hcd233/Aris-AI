1efb5a1543f2d067155e0b064cfe584eee18973a
centonhuang <centonhuang@tencent.com>
2024-01-27 14:50:46 +0800

    ✨ feat(api): add new port argument

--- internal/config/config.py
+++ internal/config/config.py
@@ -4,3 +4,5 @@ DEBUG_MODE = os.environ.get("DEBUG_MODE", "0") == "1"
 
 LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
 LOG_ROOT = os.environ.get("LOG_ROOT", "./log")
+
+API_PORT = int(os.environ.get("API_PORT", "8000"))
