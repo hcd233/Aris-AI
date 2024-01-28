931863fbf499309f847919f960e0d4ea6c4808da
centonhuang <centonhuang@tencent.com>
2024-01-28 22:01:10 +0800

    🦄 refactor(api): tiny refactor

--- internal/middleware/mysql/models/users.py
+++ internal/middleware/mysql/models/users.py
@@ -12,9 +12,9 @@ class UserSchema(BaseSchema):
 
     __tablename__ = "users"
     uid: int = Column(Integer, primary_key=True, autoincrement=True)
-    create_at = Column(DateTime, default=datetime.now)
-    update_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-    delete_at = Column(DateTime, nullable=True)
+    create_at: datetime = Column(DateTime, default=datetime.now)
+    update_at: datetime = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    delete_at: datetime = Column(DateTime, nullable=True)
     user: str = Column(String(255), nullable=False)
     password: str = Column(String(255), nullable=False)
     is_admin: bool = Column(Boolean, nullable=False, default=False)
