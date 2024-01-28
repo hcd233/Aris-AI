ea99f1ec2b99f97c93c62e33b705e42bb12b73e5
centonhuang <centonhuang@tencent.com>
2024-01-28 19:39:15 +0800

    ✨ feat(api): support list and delete api key

--- internal/api/router/v1/user.py
+++ internal/api/router/v1/user.py
@@ -55,6 +55,22 @@ def login_user(request: UserRequest) -> StandardResponse:
     return StandardResponse(code=0, status="success", message="Login successfully", data=data)
 
 
+@user_router.get("/key/list", dependencies=[Depends(jwt_auth)])
+def get_api_key_list(uid: int = Depends(jwt_auth)) -> StandardResponse:
+    with session() as conn:
+        query = (
+            conn.query(ApiKeySchema.api_key_secret, ApiKeySchema.create_at, ApiKeySchema.delete_at)
+            .filter(ApiKeySchema.uid == uid)
+            .filter(or_(ApiKeySchema.delete_at.is_(None), ApiKeySchema.delete_at > datetime.datetime.now()))
+        )
+        result = query.all()
+
+    fields = ("api_key_secret", "create_at", "expire_at")
+    data = {"uid": uid, "api_key_list": [dict(zip(fields, row)) for row in result]}
+
+    return StandardResponse(code=0, status="success", message="List api key successfully", data=data)
+
+
 @user_router.post("/key/generate", dependencies=[Depends(jwt_auth)])
 def generate_api_key(uid: int = Depends(jwt_auth)) -> StandardResponse:
     with session() as conn:
@@ -92,3 +108,25 @@ def generate_api_key(uid: int = Depends(jwt_auth)) -> StandardResponse:
         message="Generate api key successfully. Please save it carefully.",
         data=data,
     )
+
+
+@user_router.delete("/key/delete", dependencies=[Depends(jwt_auth)])
+def delete_api_key(uid: int = Depends(jwt_auth), api_key_secret: str = "") -> StandardResponse:
+    with session() as conn:
+        query = conn.query(ApiKeySchema.api_key_secret).filter(ApiKeySchema.api_key_secret == api_key_secret).filter(ApiKeySchema.uid == uid)
+        result = query.first()
+
+    if not result:
+        return StandardResponse(code=1, status="error", message="Key not exist")
+
+    with session() as conn:
+        if not conn.is_active:
+            conn.rollback()
+
+        conn.query(ApiKeySchema).filter(ApiKeySchema.api_key_secret == api_key_secret).update({ApiKeySchema.delete_at: datetime.datetime.now()})
+        conn.query(UserSchema).filter(UserSchema.uid == uid).filter(
+            or_(UserSchema.delete_at.is_(None), datetime.datetime.now() < UserSchema.delete_at)
+        ).update({UserSchema.ak_num: UserSchema.ak_num - 1})
+        conn.commit()
+
+    return StandardResponse(code=0, status="success", message="Delete api key successfully")
