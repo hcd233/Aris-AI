9ee7b4a1eab5f8e35fcb9069628a43f75848e1c1
centonhuang <centonhuang@tencent.com>
2024-01-30 23:26:15 +0800

    ✨ feat(api): support session control

--- internal/api/router/v1/session.py
+++ internal/api/router/v1/session.py
@@ -1,4 +1,7 @@
+import datetime
+
 from fastapi import APIRouter, Depends
+from sqlalchemy import or_
 
 from internal.middleware.mysql import session
 from internal.middleware.mysql.models import SessionSchema
@@ -7,6 +10,63 @@ from ...auth import sk_auth
 from ..base import StandardResponse
 
 session_router = APIRouter(prefix="/session", tags=["session"])
+
+
+@session_router.get("/list", response_model=StandardResponse, dependencies=[Depends(sk_auth)])
+async def list_session(page_id: int = 0, per_page_num: int = 20, uid: int = Depends(sk_auth)):
+    with session() as conn:
+        if not conn.is_active:
+            conn.rollback()
+            conn.close()
+
+        query = (
+            conn.query(SessionSchema.session_id, SessionSchema.create_at, SessionSchema.update_at)
+            .filter(SessionSchema.uid == uid)
+            .filter(or_(SessionSchema.delete_at.is_(None), datetime.datetime.now() < SessionSchema.delete_at))
+            .order_by(SessionSchema.create_at.desc())
+            .offset(page_id * per_page_num)
+        )
+        result = query.limit(per_page_num).all()
+
+    data = {
+        "session_list": [
+            {
+                "session_id": session_id,
+                "create_at": create_at,
+                "last_chat_at": update_at,
+            }
+            for session_id, create_at, update_at in result
+        ]
+    }
+    return StandardResponse(code=0, status="success", message="Get session list successfully", data=data)
+
+
+@session_router.get("/{session_id}", response_model=StandardResponse, dependencies=[Depends(sk_auth)])
+async def get_session(session_id: str, uid: int = Depends(sk_auth)):
+    with session() as conn:
+        if not conn.is_active:
+            conn.rollback()
+            conn.close()
+
+        query = (
+            conn.query(SessionSchema.session_id, SessionSchema.conversation, SessionSchema.create_at, SessionSchema.update_at)
+            .filter(SessionSchema.session_id == session_id)
+            .filter(SessionSchema.uid == uid)
+            .filter(or_(SessionSchema.delete_at.is_(None), datetime.datetime.now() < SessionSchema.delete_at))
+        )
+        result = query.first()
+
+    if not result:
+        return StandardResponse(code=1, status="error", message="Session not exist")
+
+    session_id, conversation, create_at, update_at = result
+    data = {
+        "session_id": session_id,
+        "history": conversation,
+        "create_at": create_at,
+        "last_chat_at": update_at,
+    }
+    return StandardResponse(code=0, status="success", message="Get session successfully", data=data)
 
 
 @session_router.post("/new", response_model=StandardResponse, dependencies=[Depends(sk_auth)])
@@ -22,3 +82,32 @@ async def new_session(uid: int = Depends(sk_auth)):
         data = {"session_id": _session.session_id, "create_at": _session.create_at}
 
     return StandardResponse(code=0, status="success", message="Create session successfully", data=data)
+
+
+@session_router.delete("/delete", response_model=StandardResponse, dependencies=[Depends(sk_auth)])
+async def delete_session(session_id: str, uid: int = Depends(sk_auth)):
+    with session() as conn:
+        if not conn.is_active:
+            conn.rollback()
+            conn.close()
+
+        query = (
+            conn.query(SessionSchema.session_id, SessionSchema.delete_at)
+            .filter(SessionSchema.session_id == session_id)
+            .filter(SessionSchema.uid == uid)
+            .filter(or_(SessionSchema.delete_at.is_(None), datetime.datetime.now() < SessionSchema.delete_at))
+        )
+
+        if not query.first():
+            return StandardResponse(code=1, status="error", message="Session not exist")
+
+        query = (
+            conn.query(SessionSchema)
+            .filter(SessionSchema.session_id == session_id)
+            .filter(SessionSchema.uid == uid)
+            .filter(or_(SessionSchema.delete_at.is_(None), datetime.datetime.now() < SessionSchema.delete_at))
+        )
+        query.update({SessionSchema.delete_at: datetime.datetime.now()})
+        conn.commit()
+
+    return StandardResponse(code=0, status="success", message="Delete session successfully")
