86c04fbf434ab91d3a4b9ad6d048ced74aff9dfd
centonhuang <centonhuang@tencent.com>
2024-02-01 00:30:12 +0800

    🦄 refactor(api): add key router to control key

--- internal/api/router/v1/__init__.py
+++ internal/api/router/v1/__init__.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 
+from .key import key_router
 from .session import session_router
 from .user import user_router
 
 v1_router = APIRouter(prefix="/v1")
+
 v1_router.include_router(user_router)
+v1_router.include_router(key_router)
 v1_router.include_router(session_router)
