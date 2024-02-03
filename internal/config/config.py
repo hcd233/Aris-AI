33cdf8fce712c97b2ebe8a200be1d632608ca83a
centonhuang <centonhuang@tencent.com>
2024-02-03 18:03:51 +0800

    🦄 refactor(api): add api host env

--- internal/config/config.py
+++ internal/config/config.py
@@ -5,6 +5,7 @@ DEBUG_MODE = os.environ.get("DEBUG_MODE", "0") == "1"
 LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
 LOG_ROOT = os.environ.get("LOG_ROOT", "./log")
 
+API_HOST = os.environ.get("API_HOST")
 API_PORT = int(os.environ.get("API_PORT", "8000"))
 
 MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE")
