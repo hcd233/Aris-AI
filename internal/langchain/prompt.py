2e07c7441238427eb991b9c342684d4053b686a1
centonhuang <centonhuang@tencent.com>
2024-02-12 16:39:45 +0800

    🦄 refactor(api): refactor chat prompt

--- internal/langchain/prompt.py
+++ internal/langchain/prompt.py
@@ -16,19 +16,27 @@ chat_template = """\
 """
 
 
-def init_str_prompt(sys_name: str, sys_prompt: str, user_name: str, ai_name: str) -> PromptTemplate:
-    input_variables = ["user_prompt", "history"]
-    template = chat_template.format(
-        sys_name=sys_name,
-        sys_prompt=sys_prompt,
-        user_name=user_name,
-        ai_name=ai_name,
-        **{var: f"{{{var}}}" for var in input_variables},  # not replace
-    )
+def init_chat_prompt(
+    sys_prompt: str, request_type: Literal["message", "string"], sys_name: str, user_name: str, ai_name: str
+) -> ChatPromptTemplate | PromptTemplate:
+    match request_type:
+        case "message":
+            chat_prompt: ChatPromptTemplate = SystemMessage(content=sys_prompt) + MessagesPlaceholder(variable_name="history") + "{user_prompt}"
+        case "string":
+            input_variables = ["user_prompt", "history"]
+            template = chat_template.format(
+                sys_name=sys_name,
+                sys_prompt=sys_prompt,
+                user_name=user_name,
+                ai_name=ai_name,
+                **{var: f"{{{var}}}" for var in input_variables},  # not replace
+            )
 
-    chat_prompt = PromptTemplate(
-        input_variables=input_variables,
-        template=template,
-    )
-
+            chat_prompt = PromptTemplate(
+                input_variables=input_variables,
+                template=template,
+            )
+        case _:
+            raise ValueError(f"Invalid request_type: {request_type}")
     return chat_prompt
+
